f4560bb9aaf4fb263ce6886e36576b47889d18e4
CerulaeanP <62661314+CerulaeanP@users.noreply.github.com>
2020-07-10 17:00:20 +0800

    最后一版修改，无果，偏差仍然很大，无法理解原因

---
+++ workcode/SARIMA/model2.0.py
@@ -0,0 +1,77 @@
+import numpy as np                               # vectors and matrices
+import pandas as pd                              # tables and data manipulations
+import matplotlib.pyplot as plt                  # plots
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+from itertools import product                    # some useful functions
+import warnings                                  # do not disturbe mode
+warnings.filterwarnings('ignore')
+class modelSARIMA:
+    # MAPE
+    def mean_absolute_percentage_error(y_true, y_pred):
+      return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+    def tsplot(y, lags=None, figsize=(12, 7), style='bmh'):
+        """
+            Plot time series, its ACF and PACF, calculate Dickey–Fuller test
+
+            y - timeseries
+            lags - how many lags to include in ACF, PACF calculation
+        """
+
+        if not isinstance(y, pd.Series):
+            y = pd.Series(y)
+
+        with plt.style.context(style):
+            fig = plt.figure(figsize=figsize)
+            layout = (2, 2)
+            ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+            acf_ax = plt.subplot2grid(layout, (1, 0))
+            pacf_ax = plt.subplot2grid(layout, (1, 1))
+
+            y.plot(ax=ts_ax)
+            p_value = sm.tsa.stattools.adfuller(y)[1]
+            ts_ax.set_title('Time Series Analysis Plots\n Dickey-Fuller: p={0:.5f}'.format(p_value))
+            smt.graphics.plot_acf(y, lags=lags, ax=acf_ax)
+            smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax)
+            plt.tight_layout()
+
+    tem = pd.read_csv('tem.csv', index_col=['DATE'], parse_dates=['DATE'])
+
+    best_model=sm.tsa.statespace.SARIMAX(tem.TMAX, order=(0, 1, 3),
+                                         seasonal_order=(0, 1, 1, 22),
+                                         enforce_stationarity=False,
+                                         enforce_invertibility=False).fit(disp=-1)
+
+    print(best_model.summary())
+
+    tsplot(best_model.resid[22+1:], lags=60)
+    plt.show()
+
+
+    def plotSARIMA(series, model, n_steps):
+        # adding model values
+        data = series.copy()
+        data.columns = ['daily','actual']
+        data['sarima_model'] = model.fittedvalues
+        # making a shift on s+d steps, because these values were unobserved by the model
+        # due to the differentiating
+        data['sarima_model'][:22+1] = np.NaN
+
+        # forecasting on n_steps forward
+        forecast = model.predict(start=data.shape[0], end=data.shape[0] + n_steps)
+        forecast = data.sarima_model.append(forecast)
+        # calculate error, again having shifted on s+d steps from the beginning
+        error = mean_absolute_percentage_error(data['actual'][22+1:], data['sarima_model'][22+1:])
+
+        plt.figure(figsize=(15, 7))
+        plt.title("Mean Absolute Percentage Error: {0:.2f}%".format(error))
+        plt.plot(forecast, color='r', label="model")
+        plt.axvspan(data.index[-1], forecast.index[-1], alpha=0.5, color='lightgrey')
+        plt.plot(data.actual, label="actual")
+        plt.legend()
+        plt.grid(True)
+
+    plotSARIMA(tem, best_model, 7)
+    plt.show()
