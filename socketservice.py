205ce3ad357ab343d5d3b32c44a5f1dc56ca75f4
yinheyinhe <64187406+yinheyinhe@users.noreply.github.com>
2020-07-10 11:28:45 +0800

    备份

--- socketservice.py
+++ socketservice.py
@@ -17,30 +17,26 @@ while True:
     print("listen for client...")
     conn, addr = sock.accept()
     print("get client")
-    print(addr)
 
-    conn.settimeout(30)
-    szBuf = conn.recv(1024)
-    print("recv:" + str(szBuf, 'gbk'))
 
-    if "0" == szBuf:
-        conn.send(b"exit")
-    else:
-        conn.send(b"welcome client\n")
+    while True:
+        conn.settimeout(30)
+        szBuf = conn.recv(1024)
+        print("recv:" + str(szBuf, 'gbk'))
 
-    filename = r"D:\pythonfiles\4.png"
-    #获取当前文件的文件名
-    basefilename = os.path.basename(filename)
-    #打开文件
-    myfile = open(filename, 'rb')
-    myfile_size = os.path.getsize(filename)
-    print(basefilename)
-    #data = {'filename': basefilename, 'filesize': myfile_size}
-    #json_obj = json.dumps(data)
-    #print(data)
-    #conn.send(json_obj.encode())
-    #conn.send(b"\n")
-    for readline in myfile:
-        conn.send(readline)
+        if "0" == szBuf:
+            conn.send(b"exit")
+        else:
+            conn.send(b"welcome client\n")
+
+        szBuf = conn.recv(1024)
+        print(str(szBuf,'gbk'))
+        filename = r"D:\pythonfiles\4.png"
+        myfile = open(filename, 'rb')
+        myfile_size = os.path.getsize(filename)
+
+        for readline in myfile:
+            conn.send(readline)
+
     conn.close()
     print("end of servive")
